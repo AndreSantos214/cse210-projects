9a495c1700a45f79ad2e5480805e162b2d93a26b
André Santos <andressantos214@gmail.com>
2024-09-28 03:32:32 +0100

    writing code Journal

--- prepare/Learning02/Journal.cs
+++ prepare/Learning02/Journal.cs
@@ -1,9 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
 public class Journal
 {
   public List<Entry> _entries;
   public void AddEntry(Entry newEntry)
   {
-
+    _entries.Add(newEntry);
   }
 
   public void DisplayAll()
