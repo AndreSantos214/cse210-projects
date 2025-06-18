f62a942373aa9ae085ec559e177cdcaf6b4f5f34
André Santos <andressantos214@gmail.com>
2025-06-18 14:47:56 +0100

    I added the description of the Exceeding Requirements

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,3 +1,4 @@
+//I changed some words in the menu to make it clearer for the user. I also created a 'Level Up' function for every 1000 points, and added some fun titles that change when the user levels up. 
 using System;
 
 class Program
